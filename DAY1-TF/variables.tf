7ff0b3e6f296e525657b61dd578348834d2d5d71
Anil Choubey <anil_choubey@hotmail.com>
2024-10-25 05:28:52 +0530

    variable file added

---
+++ DAY1-TF/variables.tf
@@ -0,0 +1,10 @@
+variable "filename" {
+  type = string
+  default = "c:/defaultvalue.txt"
+  
+}
+
+variable "content" {
+  type = string
+  default = "This is default content if some dont provide it"
+}
