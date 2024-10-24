6810cd4cbbee36cb48253a7711198c3b659cbe97
Anil Choubey <anil_choubey@hotmail.com>
2024-10-25 05:19:08 +0530

    tracking changes on the way

--- DAY1-TF/docker.tf
+++ DAY1-TF/docker.tf
@@ -1,29 +1,29 @@
-terraform {
+# terraform {
 
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
-  }
-}
+#   required_providers {
+#     docker = {
+#       source  = "kreuzwerker/docker"
+#       version = "3.0.2"
+#     }
+#   }
+# }
 
-provider "docker" {
+# provider "docker" {
 
-}
+# }
 
-resource "docker_image" "nginx1" {
-  name = "nginx:latest"
+# resource "docker_image" "nginx1" {
+#   name = "nginx:latest"
 
 
-}
+# }
 
-resource "docker_container" "nginx1" {
-  image = docker_image.nginx1.image_id
-  name  = "myNginx-server"
+# resource "docker_container" "nginx1" {
+#   image = docker_image.nginx1.image_id
+#   name  = "myNginx-server"
 
-  ports {
-    internal = 80
-    external = 8080
-  }
-}
+#   ports {
+#     internal = 80
+#     external = 8080
+#   }
+# }
---
+++ DAY1-TF/main.tf
@@ -0,0 +1,6 @@
+resource "local_file" "devops"{
+
+filename = "c:/test1.txt"
+content = "variable testing"
+}
+
