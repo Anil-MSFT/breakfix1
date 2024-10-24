5d2b928e76173d777b6be25cfa587b011fff8937
Anil Choubey <anil_choubey@hotmail.com>
2024-10-25 04:21:28 +0530

    first docker container

---
+++ DAY1-TF/docker.tf
@@ -0,0 +1,29 @@
+terraform {
+    
+  required_providers {
+    docker ={
+        source = "kreuzwerker/docker"
+        version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  
+}
+
+resource "docker_image" "nginx1" {
+    name = "nginx:latest"
+
+  
+}
+
+resource "docker_container" "nginx1" {
+    image = docker_image.nginx1.image_id
+    name = "myNginx-server"
+
+  ports{
+    internal = 80
+    external = 8080
+  }
+}
